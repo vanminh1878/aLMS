af12ccb3702546a431cd7af8cf1cda212cecab5b
minh <22520869@gm.uit.edu.vn>
2025-10-17 18:57:14 +0700

    feature: Add Account Configuration

---
+++ aLMS.Infrastructure/AccountInfra/AccountConfiguration.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using aLMS.Domain.AccountEntity;
+
+namespace aLMS.Infrastructure.AccountInfra
+{
+    public class AccountConfigurations : IEntityTypeConfiguration<Account>
+    {
+        public void Configure(EntityTypeBuilder<Account> builder)
+        {
+            builder.ToTable("account");
+
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id)
+                .HasColumnType("uuid")
+                .HasDefaultValueSql("gen_random_uuid()");
+
+            builder.Property(x => x.Username)
+                .IsRequired()
+                .HasMaxLength(50)
+                .HasColumnType("varchar(50)");
+
+            builder.HasIndex(x => x.Username)
+                .IsUnique();
+
+            builder.Property(x => x.Password)
+                .IsRequired()
+                .HasMaxLength(255)
+                .HasColumnType("varchar(255)");
+        }
+    }
+}
