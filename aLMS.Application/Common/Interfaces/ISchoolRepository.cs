e4e9baba7ba81addec28946028b9b05c3f360702
minh <22520869@gm.uit.edu.vn>
2025-10-24 19:03:47 +0700

    BE-feature: Add School Repository

---
+++ aLMS.Application/Common/Interfaces/ISchoolRepository.cs
@@ -0,0 +1,19 @@
+﻿using aLMS.Domain.SchoolEntity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aLMS.Application.Common.Interfaces
+{
+    public interface ISchoolRepository
+    {
+        Task<List<School>> GetAllAsync(CancellationToken cancellationToken = default);
+        Task<School?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+        Task AddAsync(School school, CancellationToken cancellationToken = default);
+        Task UpdateAsync(School school, CancellationToken cancellationToken = default);
+        Task DeleteAsync(Guid id, CancellationToken cancellationToken = default);
+        Task<bool> ExistsByNameAsync(string name, CancellationToken cancellationToken = default);
+    }
+}
---
+++ aLMS.Infrastructure/SchoolInfra/SchoolRepository.cs
@@ -0,0 +1,67 @@
+﻿using aLMS.Application.Common.Interfaces;
+using aLMS.Domain.SchoolEntity;
+using aLMS.Infrastructure.Common;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aLMS.Infrastructure.SchoolInfra
+{
+    public class SchoolRepository : ISchoolRepository
+    {
+        private readonly aLMSDbContext _context;
+
+        public SchoolRepository(aLMSDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<School>> GetAllAsync(CancellationToken cancellationToken = default)
+        {
+            return await _context.Schools
+                .Include(s => s.Grades)
+                .Include(s => s.Users)
+                .OrderBy(s => s.Name)
+                .ToListAsync(cancellationToken);
+        }
+
+        public async Task<School?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default)
+        {
+            return await _context.Schools
+                .Include(s => s.Grades)
+                .Include(s => s.Users)
+                .FirstOrDefaultAsync(s => s.Id == id, cancellationToken);
+        }
+
+        public async Task AddAsync(School school, CancellationToken cancellationToken = default)
+        {
+            await _context.Schools.AddAsync(school, cancellationToken);
+            await _context.SaveChangesAsync(cancellationToken);
+        }
+
+        public async Task UpdateAsync(School school, CancellationToken cancellationToken = default)
+        {
+            _context.Schools.Update(school);
+            await _context.SaveChangesAsync(cancellationToken);
+        }
+
+        public async Task DeleteAsync(Guid id, CancellationToken cancellationToken = default)
+        {
+            var school = await _context.Schools.FindAsync(new object[] { id }, cancellationToken);
+            if (school != null)
+            {
+                _context.Schools.Remove(school);
+                await _context.SaveChangesAsync(cancellationToken);
+            }
+        }
+
+        public async Task<bool> ExistsByNameAsync(string name, CancellationToken cancellationToken = default)
+        {
+            return await _context.Schools
+                .AnyAsync(s => s.Name.ToLower() == name.ToLower(), cancellationToken);
+        }
+    }
+}
